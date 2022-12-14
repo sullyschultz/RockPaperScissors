7fc3227c4e7c1cd62fc59e8006976d86f79dfc64
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-14 09:15:40 +0100

    randomChoice for computer

---
+++ script.js
@@ -0,0 +1,18 @@
+// Make a function for getting computer choice
+// Make the choice a random between rock, paper and scissor
+function getComputerChoice () {
+    const choice = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random()* choice.length);
+    return choice[randomChoice];
+}
+
+
+
+
+
+
+
+// Make a function for playing a round, taking in computer choice and player choice
+// Make an input for player choice
+// Declare rules for winning in variables ?
+// 
