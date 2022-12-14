5962466c93f4ac1ebdf36bf80a1e3e01872eb49f
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-14 23:30:40 +0100

    Made function for playRound computerChoice

--- script.js
+++ script.js
@@ -2,17 +2,41 @@
 // Make the choice a random between rock, paper and scissor
 function getComputerChoice () {
     const choice = ["rock", "paper", "scissors"];
-    const randomChoice = Math.floor(Math.random()* choice.length);
-    return choice[randomChoice];
-}
-
-
-
-
-
-
-
+    return choice[Math.floor(Math.random()* choice.length)];
+    }
 // Make a function for playing a round, taking in computer choice and player choice
+let input = prompt("Write Rock, Paper or Scissors");
+function playRound() {
+    let playerSelection = "input";
+    let computerSelection = getComputerChoice();
+   
+    if (playerSelection === computerSelection) {
+        return "Its a Tie!";
+    }
+
+    else if (
+        (playerSelection === "rock" && computerSelection == "scissors") ||
+        (playerSelection === "paper" && computerSelection == "rock") || 
+        (playerSelection === "scissors" && computerSelection == "paper")
+        ) {
+            return "You win!";
+        }
+        else {
+            return "You lose!";
+        }
+    } 
+        
+    function game() {
+
+    }
+
+     
+
+    
+     
+     
+     
+     
+
 // Make an input for player choice
-// Declare rules for winning in variables ?
-// 
+
