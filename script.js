747e28d7448fb4f7982918d95a246b1495c3359d
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-15 14:26:06 +0100

    Functions and logs

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ function getComputerChoice () {
     const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random()* choice.length)];
     }
+// Make a function for playerSelection
+function getPlayerChoice () {
+    let input = prompt("Write rock, paper or scissors");
+    while (input == null) { 
+        input = prompt("Write rock, paper or scissors");
+    }
+    input = input.toLowerCase();
+    return input; 
+}
+    
+
 // Make a function for playing a round, taking in computer choice and player choice
-let input = prompt("Write Rock, Paper or Scissors");
+
 function playRound() {
-    let playerSelection = "input";
+    let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
-   
+    let result;
     if (playerSelection === computerSelection) {
-        return "Its a Tie!";
+        result = "Its a Tie!";
+        console.log( "You: ", playerSelection);
+        console.log( "Computer: ", computerSelection);
+        console.log(result);
+        //return result;
     }
 
     else if (
@@ -19,15 +34,25 @@ function playRound() {
         (playerSelection === "paper" && computerSelection == "rock") || 
         (playerSelection === "scissors" && computerSelection == "paper")
         ) {
-            return "You win!";
+            result = "You win!";
+            console.log( "You: ", playerSelection);
+            console.log( "Computer: ", computerSelection);
+            console.log(result);
+            
         }
         else {
-            return "You lose!";
+            result = "You lose!";
+            console.log( "You: ", playerSelection);
+            console.log( "Computer: ", computerSelection);
+            console.log(result);
+            
         }
     } 
-        
+    // Make a function that plays 5 rounds    
     function game() {
-
+        for (let i = 0; i <= 4; i++) {
+        playRound();
+        }
     }
 
      
