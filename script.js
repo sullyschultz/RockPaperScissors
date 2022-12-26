a9f348240ea7716c3b0c05d97ce68e29de048d6c
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-26 22:45:31 +0100

    Added Buttons for playing, and divs/paragraphs for viewing score

--- script.js
+++ script.js
@@ -1,62 +1,113 @@
-// Make a function for getting computer choice
-// Make the choice a random between rock, paper and scissor
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+let playerPoints = document.getElementById("playerScore");
+let computerPoints = document.getElementById("computerScore");
+let ties = document.getElementById("tieScore");
+
 function getComputerChoice () {
-    const choice = ["rock", "paper", "scissors"];
+    const choice = ["rock", "paper", "scissor"];
     return choice[Math.floor(Math.random()* choice.length)];
     }
-// Make a function for playerSelection
-function getPlayerChoice () {
-    let input = prompt("Write rock, paper or scissors");
-    while (input == null || input == "") { 
-        input = prompt("Write rock, paper or scissors");
-    }
-    input = input.toLowerCase();
-    return input; 
 
-}
-// Make a function for playing a round, taking in computer choice and player choice
+ /*function getPlayerChoice () {
+      let input = prompt("Write rock, paper or scissors");
+     while (input == null || input == "") { 
+         input = prompt("Write rock, paper or scissors");
+     }
+     input = input.toLowerCase();
+     return input; 
 
-function playRound() {
-    let playerSelection = getPlayerChoice();
+} */
+
+const rockBtn = document.getElementById("rock").addEventListener("click", (e) => {
+    e = "rock";
+    playRound(e);
+})
+const paperBtn = document.getElementById("paper").addEventListener("click", (e) => {
+    e = "paper";
+    playRound(e);
+})
+const scissorBtn = document.getElementById("scissor").addEventListener("click", (e) => {
+    e = "scissor";
+    playRound(e);
+})
+
+let round = 1;
+let result = "";
+let roundResult = document.getElementById("gameResult");
+
+
+function playRound(e) {
+    let playerSelection = e;
     let computerSelection = getComputerChoice();
-    let result;
     if (playerSelection === computerSelection) {
         result = "Its a Tie!";
+        roundResult.innerHTML= "Round: " + round + " " + result;
+        /* console.log( "Round: ", round);
         console.log( "You: ", playerSelection);
         console.log( "Computer: ", computerSelection);
         console.log(result);
+        console.log("-----------------------"); */
+        round ++; 
+        tieScore ++;
+        ties.innerHTML = "Ties: " + tieScore;
+       
         
     }
 
     else if (
-        (playerSelection === "rock" && computerSelection == "scissors") ||
+        (playerSelection === "rock" && computerSelection == "scissor") ||
         (playerSelection === "paper" && computerSelection == "rock") || 
-        (playerSelection === "scissors" && computerSelection == "paper")
+        (playerSelection === "scissor" && computerSelection == "paper")
         ) {
             result = "You win!";
+            roundResult.innerHTML= "Round: " + round + " " + result;
+         /*   console.log( "Round: ", round);
             console.log( "You: ", playerSelection);
             console.log( "Computer: ", computerSelection);
             console.log(result);
+            console.log("-----------------------"); */
+            round ++; 
+            playerScore ++;
+            playerPoints.innerHTML = "Your Points: " + playerScore;
             
         }
         else {
             result = "You lose!";
+            roundResult.innerHTML= "Round: " + round + " " + result;
+          /*  console.log( "Round: ", round);
             console.log( "You: ", playerSelection);
             console.log( "Computer: ", computerSelection);
             console.log(result);
+            console.log("-----------------------"); */
+            round ++; 
+            computerScore ++;
+            computerPoints.innerHTML = "Computer Points: " + computerScore;
+           
             
         }
     } 
-    // Make a function that plays 5 rounds  
-    // Make a log for round  
-    function game() {
 
-            for (let i = 0; i <= 4; i++) {
-            console.log("Round " + (i+1));
-            playRound();
-            console.log("-----------------------");
-        }
-    }
+
+
+    
+    
+
+
+    
+    
+   
+        
+    
+   // function game() {
+
+         //   for (let i = 0; i <= 4; i++) {
+        //  console.log("Round " + (i+1));
+          //  playRound();
+           // console.log("-----------------------");
+      //  }
+  //  }
 
      
 
